5b06c6efbbf9a7a474a148ab0db75b643899864b
abhizeit <abhijeitsarraf@gmail.com>
2023-02-09 18:10:32 +0530

    final commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 const connect = async () => {
-  return mongoose.connect(process.env.DB_URL);
+  return mongoose.connect(
+    process.env.DB_URL || "mongodb://127.0.0.1:27017/dailyBlog"
+  );
 };
 
 module.exports = connect;
